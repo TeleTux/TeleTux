d6cea66827e7feac12e58d2514acf81981d31e41
freetux <thefreetux@gmail.com>
2022-05-20 18:13:21 +0430

    update PersianDateFormat

--- TMessagesProj/src/main/java/tw/nekomimi/nekogram/shamsicalendar/PersianDateFormat.java
+++ TMessagesProj/src/main/java/tw/nekomimi/nekogram/shamsicalendar/PersianDateFormat.java
@@ -280,4 +280,6 @@ public class PersianDateFormat {
     return value.replace("1", "۱").replace("2", "۲").replace("3", "۳").replace("4", "۴").replace("5", "۵")
       .replace("6", "۶").replace("7", "۷").replace("8", "۸").replace("9", "۹").replace("0", "۰");
   }
+
+
 }
