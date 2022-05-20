7e26072c75228aa6aa75b8fed6a00bc202f7db68
freetux <thefreetux@gmail.com>
2022-05-20 15:45:37 +0430

    Update PersianDateFormat.java

--- TMessagesProj/src/main/java/tw/nekomimi/nekogram/shamsicalendar/PersianDateFormat.java
+++ TMessagesProj/src/main/java/tw/nekomimi/nekogram/shamsicalendar/PersianDateFormat.java
@@ -1,58 +1,106 @@
+package com.freetux.PRSMS.common.util.shamsicalendar;
 
-package tw.nekomimi.nekogram.shamsicalendar;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Date;
 
 /**
- * Created by Saman on 3/31/2017 AD.
+ * Created by SamanZaman(saman.zamani1@gmail.com) on 3/31/2017 AD.
+ *
+ * Last update on Sunday, November 17, 2021
  */
 
 public class PersianDateFormat {
+  //number Format
+  public enum PersianDateNumberCharacter {
+    ENGLISH,
+    FARSI
+  }
   //variable
   /**
    * Key for convert Date to String
    */
-  private String[] key = {"a", "l", "j", "F", "Y", "H", "i", "s", "d", "g", "n", "m", "t", "w", "y",
-      "z", "A",
-      "L","X","C","E","T","b","D","e","B","S"};
-  private String pattern;
+  private final String[] key = {"a", "l", "j", "F", "Y", "H", "i", "s", "d", "g", "n", "m", "t", "w", "y",
+          "z", "A",
+          "L","X","C","E"};
+  //from version 1.3.3 pattern is public and has de
+  private String pattern = "l j F Y H:i:s";
+  private PersianDateNumberCharacter numberCharacter = PersianDateNumberCharacter.ENGLISH;
+
   /**
    * key_parse for convert String to PersianDate
    *
    * yyyy = Year (1396) MM = month (02-12-...) dd = day (13-02-15-...) HH = Hour (13-02-15-...) mm =
    * minutes (13-02-15-...) ss = second (13-02-15-...)
    */
-  private String[] key_parse = {"yyyy", "MM", "dd", "HH", "mm", "ss"};
+  private final String[] key_parse = {"yyyy", "MM", "dd", "HH", "mm", "ss"};
 
   /**
-   * Constracutor
+   * Constructor for create formatter with just pattern
    */
   public PersianDateFormat(String pattern) {
     this.pattern = pattern;
   }
 
   /**
-   * initilize pattern
+   * Constructor for create pattern with number character format
+   *
+   * @param pattern pattern use for format
+   * @param numberCharacter character type can be PersianDateNumberCharacter.FARSI | PersianDateNumberCharacter.English
    */
-  public PersianDateFormat() {
-    pattern = "l j F Y H:i:s";
+  public PersianDateFormat(String pattern,PersianDateNumberCharacter numberCharacter) {
+    this.pattern = pattern;
+    this.numberCharacter = numberCharacter;
+  }
+  /**
+   * Change pattern
+   *
+   * @param pattern change format pattern
+   */
+  public void setPattern(String pattern) {
+    this.pattern = pattern;
   }
 
-
-
-
-   /**
-   * new patterns T for mounth name in latin ----- b for shortday in persian number ----- D for 4 digit year in persian number ---- e for short month in persian number
+  /**
+   * Change number character
+   *
+   * @param numberCharacter number character
    */
+  public void setNumberCharacter(
+          PersianDateNumberCharacter numberCharacter) {
+    this.numberCharacter = numberCharacter;
+  }
 
+  /**
+   * Constructor without pattern
+   */
+  public PersianDateFormat() {}
 
+  /**
+   * Format date
+   *
+   *
+   * @param date PersianDate object of date
+   * @param pattern Pattern you want to show
+   * @return date in pattern
+   */
+  public static String format(PersianDate date, String pattern){
+    return format(date,pattern, PersianDateNumberCharacter.ENGLISH);
+  }
 
-  public static String format(PersianDate date, String pattern) {
+  /**
+   * Convert with charter type
+   *
+   * @param date date in PersianDate object
+   * @param pattern pattern
+   * @param numberFormatCharacter number charter
+   * @return return date
+   */
+  public static String format(PersianDate date, String pattern, PersianDateNumberCharacter numberFormatCharacter) {
     if(pattern == null) pattern="l j F Y H:i:s";
     String[] key = {"a", "l", "j", "F", "Y", "H", "i", "s", "d", "g", "n", "m", "t", "w", "y", "z",
-        "A", "L","X","C","E","T","b","D","e","B","S"};
+            "A", "L","X","C","E"};
     String year2;
     if (("" + date.getShYear()).length() == 2) {
       year2 = "" + date.getShYear();
@@ -62,27 +110,22 @@ public class PersianDateFormat {
       year2 = ("" + date.getShYear()).substring(2, 4);
     }
     String[] values = {date.getShortTimeOfTheDay(), date.dayName(), "" + date.getShDay(),
-        date.monthName(),
-        "" + date.getShYear(),
-        textNumberFilterStatic("" + date.getHour()), textNumberFilterStatic("" + date.getMinute()),
-        textNumberFilterStatic("" + date.getSecond()),
-        textNumberFilterStatic("" + date.getShDay()), "" + date.getHour(), "" + date.getShMonth(),
-        textNumberFilterStatic("" + date.getShMonth()),
-        "" + date.getMonthDays(), "" + date.dayOfWeek(), year2, "" + date.getDayInYear(),
-        date.getTimeOfTheDay(),
-        (date.isLeap() ? "1" : "0"),
-        date.AfghanMonthName(),
-        date.KurdishMonthName(),
-        date.PashtoMonthName(),
-        date.monthNamesLatin(),
-        LanguageUtils.getPersianNumbers(String.valueOf(date.getShDay())),
-        LanguageUtils.getPersianNumbers(String.valueOf(date.getShYear())),
-        LanguageUtils.getPersianNumbers(String.valueOf( date.getShMonth())),
-        LanguageUtils.getPersianNumbers(String.valueOf(textNumberFilterStatic("" + date.getHour()))),
-        LanguageUtils.getPersianNumbers(String.valueOf(textNumberFilterStatic("" + date.getMinute()))) 
-        
-
+            date.monthName(),
+            "" + date.getShYear(),
+            textNumberFilterStatic("" + date.getHour()), textNumberFilterStatic("" + date.getMinute()),
+            textNumberFilterStatic("" + date.getSecond()),
+            textNumberFilterStatic("" + date.getShDay()), "" + date.getHour(), "" + date.getShMonth(),
+            textNumberFilterStatic("" + date.getShMonth()),
+            "" + date.getMonthDays(), "" + date.dayOfWeek(), year2, "" + date.getDayInYear(),
+            date.getTimeOfTheDay(),
+            (date.isLeap() ? "1" : "0"),
+            date.AfghanMonthName(),
+            date.KurdishMonthName(),
+            date.PashtoMonthName()
     };
+    if(numberFormatCharacter == PersianDateNumberCharacter.FARSI){
+      PersianDateFormat.farsiCharacter(values);
+    }
     for (int i = 0; i < key.length; i++) {
       pattern = pattern.replace(key[i], values[i]);
     }
@@ -90,7 +133,7 @@ public class PersianDateFormat {
   }
 
   public String format(PersianDate date) {
-    String year2 = null;
+    String year2;
     if (("" + date.getShYear()).length() == 2) {
       year2 = "" + date.getShYear();
     } else if (("" + date.getShYear()).length() == 3) {
@@ -99,30 +142,27 @@ public class PersianDateFormat {
       year2 = ("" + date.getShYear()).substring(2, 4);
     }
     String[] values = {date.isMidNight() ? "ق.ظ" : "ب.ظ", date.dayName(), "" + date.getShDay(),
-        date.monthName(),
-        "" + date.getShYear(),
-        this.textNumberFilter("" + date.getHour()), this.textNumberFilter("" + date.getMinute()),
-        this.textNumberFilter("" + date.getSecond()),
-        this.textNumberFilter("" + date.getShDay()), "" + date.getHour(), "" + date.getShMonth(),
-        this.textNumberFilter("" + date.getShMonth()),
-        "" + date.getMonthDays(), "" + date.dayOfWeek(), year2, "" + date.getDayInYear(),
-        date.getTimeOfTheDay(),
-        (date.isLeap() ? "1" : "0"),
-        date.AfghanMonthName(),
-        date.KurdishMonthName(),
-        date.PashtoMonthName(),
-        date.monthNamesLatin(),
-        LanguageUtils.getPersianNumbers(String.valueOf(date.getShDay())),
-        LanguageUtils.getPersianNumbers(String.valueOf(date.getShYear())),
-        LanguageUtils.getPersianNumbers(String.valueOf( date.getShMonth())),
-        LanguageUtils.getPersianNumbers(String.valueOf(textNumberFilterStatic("" + date.getHour()))),
-        LanguageUtils.getPersianNumbers(String.valueOf(textNumberFilterStatic("" + date.getMinute()))) 
+            date.monthName(),
+            "" + date.getShYear(),
+            this.textNumberFilter("" + date.getHour()), this.textNumberFilter("" + date.getMinute()),
+            this.textNumberFilter("" + date.getSecond()),
+            this.textNumberFilter("" + date.getShDay()), "" + date.getHour(), "" + date.getShMonth(),
+            this.textNumberFilter("" + date.getShMonth()),
+            "" + date.getMonthDays(), "" + date.dayOfWeek(), year2, "" + date.getDayInYear(),
+            date.getTimeOfTheDay(),
+            (date.isLeap() ? "1" : "0"),
+            date.AfghanMonthName(),
+            date.KurdishMonthName(),
+            date.PashtoMonthName()
     };
+    if(this.numberCharacter == PersianDateNumberCharacter.FARSI){
+      farsiCharacter(values);
+    }
     return this.stringUtils(this.pattern, this.key, values);
   }
 
   /**
-   * Parse jalli date from String
+   * Parse Jallali date from String
    *
    * @param date date in string
    */
@@ -131,7 +171,7 @@ public class PersianDateFormat {
   }
 
   /**
-   * Parse jalli date from String
+   * Parse Jallali date from String
    *
    * @param date date in string
    * @param pattern pattern
@@ -158,12 +198,12 @@ public class PersianDateFormat {
       }
     }
     return new PersianDate()
-        .initJalaliDate(JalaliDate.get(0), JalaliDate.get(1), JalaliDate.get(2), JalaliDate.get(3),
-            JalaliDate.get(4), JalaliDate.get(5));
+            .initJalaliDate(JalaliDate.get(0), JalaliDate.get(1), JalaliDate.get(2), JalaliDate.get(4),
+                    JalaliDate.get(3), JalaliDate.get(5));
   }
 
   /**
-   * Convert String Grg date to persiand date object
+   * Convert String Grg date to persian date object
    *
    * @param date date in String
    * @return PersianDate object
@@ -173,7 +213,7 @@ public class PersianDateFormat {
   }
 
   /**
-   * Convert String Grg date to persiand date object
+   * Convert String Grg date to persian date object
    *
    * @param date date String
    * @param pattern pattern
@@ -188,7 +228,7 @@ public class PersianDateFormat {
    * Replace String
    *
    * @param text String
-   * @param key Loking for
+   * @param key Looking for
    * @param values Replace with
    */
   private String stringUtils(String text, String[] key, String[] values) {
@@ -217,9 +257,28 @@ public class PersianDateFormat {
     }
     return date;
   }
+
+  /**
+   * Convert English characters to Farsi characters
+   *
+   * @param values a string array of values
+   * @return a converted string array
+   */
+  public static String[] farsiCharacter(String[] values){
+    String[] persianChars = {"۰", "۱", "۲", "٣", "۴", "۵", "۶", "۷", "۸", "٩"};
+    String[] englishChars = {"0", "1", "2", "3", "4", "5", "6", "7", "8", "9"};
+    for (int i = 0; i < values.length; i++) {
+      String tmpValue = values[i];
+      for (int j = 0; j< persianChars.length;j++){
+        tmpValue = tmpValue.replaceAll(englishChars[j],persianChars[j]);
+      }
+      values[i] = tmpValue;
+    }
+
+    return values;
+  }
   public static String convertToPersianDigits(String value) {
     return value.replace("1", "۱").replace("2", "۲").replace("3", "۳").replace("4", "۴").replace("5", "۵")
       .replace("6", "۶").replace("7", "۷").replace("8", "۸").replace("9", "۹").replace("0", "۰");
   }
 }
-
