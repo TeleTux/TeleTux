edbfc643dcd02c3607bd068e8f7557c09ec8ea0b
freetux <thefreetux@gmail.com>
2022-05-20 13:13:44 +0430

    fix bug

--- TMessagesProj/src/main/java/org/telegram/ui/CacheControlActivity.java
+++ TMessagesProj/src/main/java/org/telegram/ui/CacheControlActivity.java
@@ -29,7 +29,7 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.FrameLayout;
 import android.widget.LinearLayout;
-
+import android.widget.TextView;
 import androidx.core.widget.NestedScrollView;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
