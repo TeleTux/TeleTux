cbd2cc5cab99e5ed837e188252259c72f7e9c6ff
freetux <thefreetux@gmail.com>
2022-05-21 07:32:27 +0430

    Update LocaleController.java

--- TMessagesProj/src/main/java/org/telegram/messenger/LocaleController.java
+++ TMessagesProj/src/main/java/org/telegram/messenger/LocaleController.java
@@ -1653,7 +1653,7 @@ public class LocaleController {
                     if ( displayPersianCalendarByLatin) {
                         return getInstance().formatterDay.format(new Date(date));
                     } else {
-                        PersianDateFormat pdformater1 = new PersianDateFormat("H:i", PersianDateNumberCharacter.FARSI);
+                        PersianDateFormat pdformater1 = new PersianDateFormat("H:i", PersianDateFormat.PersianDateFormat.PersianDateNumberCharacter.FARSI);
                         return pdformater1.format(pdate);
                     }
                 } else{
