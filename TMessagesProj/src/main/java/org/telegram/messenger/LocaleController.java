0457e8a1a07251dbb458fe20f0b7fa563e9753a3
freetux <thefreetux@gmail.com>
2022-05-21 06:41:11 +0430

    fix: show persian date time

--- TMessagesProj/src/main/java/org/telegram/messenger/LocaleController.java
+++ TMessagesProj/src/main/java/org/telegram/messenger/LocaleController.java
@@ -1653,7 +1653,7 @@ public class LocaleController {
                     if ( displayPersianCalendarByLatin) {
                         return getInstance().formatterDay.format(new Date(date));
                     } else {
-                        PersianDateFormat pdformater1 = new PersianDateFormat("B:S");
+                        PersianDateFormat pdformater1 = new PersianDateFormat("H:i", PersianDateNumberCharacter.FARSI);
                         return pdformater1.format(pdate);
                     }
                 } else{
@@ -2066,7 +2066,7 @@ public class LocaleController {
                         if (displayPersianCalendarByLatin) {
                             return getInstance().formatterDay.format(new Date(date));
                         } else {
-                            PersianDateFormat pdformater1 = new PersianDateFormat("B:S");
+                            PersianDateFormat pdformater1 = new PersianDateFormat("H:i", PersianDateNumberCharacter.FARSI);
                             return pdformater1.format(pdate);
                         }
                     } else{
